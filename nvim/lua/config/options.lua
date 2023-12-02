e010fcc14015ec84d2306f4255ae628fa299b6c1
laenzmann Mac Book <lennartbecker17@gmail.com>
2023-12-02 12:24:13 +0100

    init

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,10 @@
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = true
+opt.scrolloff = 8
+opt.tabstop = 4 
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.clipboard = "unnamedplus"
